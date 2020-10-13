3a0b1d674cb989c58d1665344bc3f1b8067e24ed
dango301 <46346469+dango301@users.noreply.github.com>
2020-10-13 21:18:55 +0200

    small visual adjustments

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,6 @@
-window.addEventListener('load', scrollNav)
-window.addEventListener('scroll', scrollNav)
 const scrollNav = () => {
     document.querySelector('header').classList.toggle('stick', window.scrollY > 0)
     document.querySelectorAll('.page-transition').forEach(el => el.classList.toggle('stick', window.scrollY > 0))
 }
+window.addEventListener('load', scrollNav)
+window.addEventListener('scroll', scrollNav)
